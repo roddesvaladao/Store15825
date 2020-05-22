eb4d0bcc67bb396c1c924f69114db3a0fc768075
roddesvaladao <65776938+roddesvaladao@users.noreply.github.com>
2020-05-22 12:18:27 -0300

    Sent by DeployFC at 22/05/2020 12:18:26

---
+++ htm/js/amh-resource-01.js
@@ -0,0 +1,39 @@
+{
+  "header-close-menu":"Close menu",
+  "header-cat-home":"Home",
+  "header-cat-releases":"Releases",
+  "header-cat-sales":"Sales",
+  "header-detailed-search":"Detailed search",
+  "header-open-menu":"Open menu",
+  "header-cat-cart":"Cart",
+  "header-search-input-button":"Search",
+  "header-search-input":"Search products",
+
+  "home-title-sales":"Products on sale",
+  "home-title-sales-more-products":"MORE PRODUCTS",
+  "home-title-releases":"Product releases",
+  "home-title-releases-more-products":"MORE PRODUCTS",
+
+  "footer-cat-home":"Home",
+  "footer-cat-my-account":"My account",
+  "footer-cat-my-orders":"My orders",
+  "footer-cat-news":"News",
+  "footer-cat-help":"Help",
+  "footer-cat-contact-us":"Contact us",
+  "footer-title-share":"Share",
+  "footer-title-share-send-email":"Send an email",
+  "footer-title-share-facebook":"Share with Facebook",
+  "footer-title-share-twitter":"Share with Twitter",
+  "footer-title-share-pinterest":"Share with Pinterest",
+  "footer-title-share-whatsapp":"Share with WhatsApp",
+  "footer-secure-site":"Secure site",
+  "footer-ehcommerce":"ehcommerce",
+
+  "details-more-details":"MORE DETAILS",
+
+  "product-not-found-title":"No products were found on this search",
+  "product-not-found-click-here":"Click here",
+  "product-not-found-click-here-text":"for more search options.",
+  "product-not-found-sales":"Products on sale"
+  
+}
