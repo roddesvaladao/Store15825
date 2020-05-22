f451c3684091032398c7eb801b9ee4761fa219a4
roddesvaladao <65776938+roddesvaladao@users.noreply.github.com>
2020-05-22 12:18:24 -0300

    Sent by DeployFC at 22/05/2020 12:18:24

---
+++ htm/js/amh-resource-00.js
@@ -0,0 +1,39 @@
+{
+  "header-close-menu":"Fechar menu",
+  "header-cat-home":"Home",
+  "header-cat-releases":"Lançamentos",
+  "header-cat-sales":"Promoções",
+  "header-detailed-search":"Busca detalhada",
+  "header-open-menu":"Abrir menu",
+  "header-cat-cart":"Carrinho",
+  "header-search-input-button":"Buscar",
+  "header-search-input":"Digite sua busca aqui",
+    
+  "home-title-sales":"Produtos em promoção",
+  "home-title-sales-more-products":"MAIS PROMOÇÕES",
+  "home-title-releases":"Lançamentos",
+  "home-title-releases-more-products":"MAIS LANÇAMENTOS",
+
+  "footer-cat-home":"Home",
+  "footer-cat-my-account":"Minha conta",
+  "footer-cat-my-orders":"Meus pedidos",
+  "footer-cat-news":"Notícias",
+  "footer-cat-help":"Ajuda",
+  "footer-cat-contact-us":"Fale conosco",
+  "footer-title-share":"Compartilhe",
+  "footer-title-share-send-email":"Envie um e-mail",
+  "footer-title-share-facebook":"Compartilhe com Facebook",
+  "footer-title-share-Twitter":"Compartilhe com Twitter",
+  "footer-title-share-pinterest":"Compartilhe com Pinterest",
+  "footer-title-share-whatsapp":"Compartilhe com WhatsApp",
+  "footer-secure-site":"Site Seguro",
+  "footer-ehcommerce":"Fastcommerce",
+
+  "details-more-details":"MAIS DETALHES",
+
+  "product-not-found-title":"Nenhum produto encontrado na busca",
+  "product-not-found-click-here":"Clique aqui",
+  "product-not-found-click-here-text":"para mais opções de busca.",
+  "product-not-found-sales":"Produtos em promoção"
+  
+}
