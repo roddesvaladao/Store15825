505cf7f4b2676cb73a4e5f696b5a00025d3ff967
roddesvaladao <65776938+roddesvaladao@users.noreply.github.com>
2020-05-22 12:18:43 -0300

    Sent by DeployFC at 22/05/2020 12:18:43

---
+++ htm/js/config.js
@@ -0,0 +1,22 @@
+/* Configurações gerais da loja */
+
+var oStoreConfig$={
+  "Product_Badges_Home_ProductList":[true,"Mostra badges na home e lista de produtos."],
+  "History_Page":[true,"Mostra histórico de páginas visitadas."],
+  "Product_SoldOut_Badge_ProductList":[true,"Mostra badge de produto esgotado na lista de produtos."],
+  "Product_SoldOut_Badge_SubProduct_ProductList":[true,"Mostra badge de subproduto esgotado na lista de produtos."],
+  "Change_Image_Hover_Home_ProductList":[true,"Ao passar o mouse, altera imagem do produto na home e lista de produtos."],
+  "Header_Voice_Search":[true,"Mostra busca por voz no campo de busca do topo."],
+  "News_Voice_Search":[true,"Mostra busca por voz na página de notícias."],
+  "Popup_Dont_Go":[false,"Mostra popup antes do visitante sair da página."],
+  "Product_Details_Show_Stock":[false,"Mostra estoque na página de detalhes dos produtos."],
+  "Product_Details_Show_Date_Promotion":[false,"Mostra data de término de promoção na página de detalhes dos produtos."],
+  "Home_Slideshow":[true,"Mostra banners rotativos na home."],
+  "Footer_Cookie_Policy":[false,"Mostra alerta política de cookies."],
+  "Next_page_button":[1,"Exibe botão para autopaginação. Informe 0 para desabilitar ou digite a quantidade de páginas de produtos a partir da qual será exibido botão para ver mais produtos."],
+  "Show_wishlist":[false,"Exibe lista de desejos."],
+  "Recommend_Product":[true,"Mostra link na página de detalhes para recomendar o produto"],
+  "Grid_Pinch_Zoom":[true,"Efeito de pinça no zoom da grade"],
+  "Cart_ZipCode_Price_Side_Cart":[true,"Desabilitar valor zero do frete no carrinho lateral"],
+  "Cart_ZipCode_Price_Shopping_Cart":[true,"Desabilitar valor zero do frete no carrinho de compras"]
+};
