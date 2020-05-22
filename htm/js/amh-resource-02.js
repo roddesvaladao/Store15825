02311936ea724561ae305526439597c80f24b5c1
roddesvaladao <65776938+roddesvaladao@users.noreply.github.com>
2020-05-22 12:18:29 -0300

    Sent by DeployFC at 22/05/2020 12:18:29

---
+++ htm/js/amh-resource-02.js
@@ -0,0 +1,39 @@
+{
+  "header-close-menu":"Cerrar menu",
+  "header-cat-home":"Home",
+  "header-cat-releases":"Lanzamientos",
+  "header-cat-sales":"Promociones",
+  "header-detailed-search":"Búsqueda detallada",
+  "header-open-menu":"Abrir menu",
+  "header-cat-cart":"Carrito",
+  "header-search-input-button":"Buscar",
+  "header-search-input":"Busca productos",
+
+  "home-title-sales":"Productos en Promoción",
+  "home-title-sales-more-products":"MÁS PROMOCIONES",
+  "home-title-releases":"Lanzamientos",
+  "home-title-releases-more-products":"MÁS LANZAMIENTOS",
+
+  "footer-cat-home":"Home",
+  "footer-cat-my-account":"Mi cuenta",
+  "footer-cat-my-orders":"Mis pedidos",
+  "footer-cat-news":"Noticias",
+  "footer-cat-help":"Ayuda",
+  "footer-cat-contact-us":"Contáctenos",
+  "footer-title-share":"Seguinos",
+  "footer-title-share-send-email":"Enviar un correo electrónico",
+  "footer-title-share-facebook":"Compartir con Facebook",
+  "footer-title-share-twitter":"Compartir con Twitter",
+  "footer-title-share-pinterest":"Compartir con Pinterest",
+  "footer-title-share-whatsapp":"Compartir con WhatsApp",
+  "footer-secure-site":"Sitio seguro",
+  "footer-ehcommerce":"ehcommerce",
+
+  "details-more-details":"MÁS DETALLES",
+  
+  "product-not-found-title":"No se encontraron productos en esta búsqueda",
+  "product-not-found-click-here":"Haga clic aquí",
+  "product-not-found-click-here-text":"para más opciones de búsqueda.",
+  "product-not-found-sales":"Productos en Promoción"
+  
+}
